880ee9cbba9bb2bf2f7093588d07130b95498bb3
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2021-10-10 13:21:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,182 @@
+var turn = 0;
+
+function putXor0one() {
+    
+    if(document.getElementById('button1').click = true && document.getElementById('button1').innerText ==""){
+        if(turn % 2 == 0) {
+            document.getElementById('button1').innerText = "X";
+            ++turn;
+        }
+        else{
+            document.getElementById('button1').innerText = "0";
+            ++turn; 
+        }
+    }
+            if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+        document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+        document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+        &&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+        document.getElementById('button9').innerText !=""){
+            document.getElementById('restart').style.visibility='visible';
+        }
+}
+ function putXor0two() {
+    
+    if(document.getElementById('button2').click = true && document.getElementById('button2').innerText ==""){
+        if(turn % 2 == 0) {
+            document.getElementById('button2').innerText = "X";
+            ++turn;
+        }
+        else{
+            document.getElementById('button2').innerText = "0";
+            ++turn; 
+        }
+    }
+    if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+}
+function putXor0three() {
+    
+    if(document.getElementById('button3').click = true && document.getElementById('button3').innerText ==""){
+        if(turn % 2 == 0) {
+            document.getElementById('button3').innerText = "X";
+            ++turn;
+        }
+        else{
+            document.getElementById('button3').innerText = "0";
+            ++turn; 
+        }
+    }
+    if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+}
+    function putXor0four() {
+    
+        if(document.getElementById('button4').click = true && document.getElementById('button4').innerText ==""){
+            if(turn % 2 == 0) {
+                document.getElementById('button4').innerText = "X";
+                ++turn;
+            }
+            else{
+                document.getElementById('button4').innerText = "0";
+                ++turn; 
+            }
+        }
+        if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+    }
+        function putXor0five() {
+    
+            if(document.getElementById('button5').click = true && document.getElementById('button5').innerText ==""){
+                if(turn % 2 == 0) {
+                    document.getElementById('button5').innerText = "X";
+                    ++turn;
+                }
+                else{
+                    document.getElementById('button5').innerText = "0";
+                    ++turn; 
+                }
+            }
+            if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+        }
+            function putXor0six() {
+    
+                if(document.getElementById('button6').click = true && document.getElementById('button6').innerText ==""){
+                    if(turn % 2 == 0) {
+                        document.getElementById('button6').innerText = "X";
+                        ++turn;
+                    }
+                    else{
+                        document.getElementById('button6').innerText = "0";
+                        ++turn; 
+                    }
+                }
+                if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+            }
+                function putXor0seven() {
+    
+                    if(document.getElementById('button7').click = true && document.getElementById('button7').innerText ==""){
+                        if(turn % 2 == 0) {
+                            document.getElementById('button7').innerText = "X";
+                            ++turn;
+                        }
+                        else{
+                            document.getElementById('button7').innerText = "0";
+                            ++turn; 
+                        }
+                    }
+                    if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+                }
+                    function putXor0eight() {
+    
+                        if(document.getElementById('button8').click = true && document.getElementById('button8').innerText ==""){
+                            if(turn % 2 == 0) {
+                                document.getElementById('button8').innerText = "X";
+                                ++turn;
+                            }
+                            else{
+                                document.getElementById('button8').innerText = "0";
+                                ++turn; 
+                            }
+                        }
+                        if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+                    }
+                        function putXor0nine() {
+    
+                            if(document.getElementById('button9').click = true && document.getElementById('button9').innerText ==""){
+                                if(turn % 2 == 0) {
+                                    document.getElementById('button9').innerText = "X";
+                                    ++turn;
+                                }
+                                else{
+                                    document.getElementById('button9').innerText = "0";
+                                    ++turn; 
+                                }
+                            }
+                            if( document.getElementById('button1').innerText !="" &&  document.getElementById('button2').innerText !="" &&
+document.getElementById('button3').innerText !="" && document.getElementById('button4').innerText !="" &&
+document.getElementById('button5').innerText !="" &&  document.getElementById('button6').innerText !=""
+&&  document.getElementById('button7').innerText !="" &&  document.getElementById('button8').innerText !="" &&
+document.getElementById('button9').innerText !=""){
+    document.getElementById('restart').style.visibility='visible';
+}
+                        }
